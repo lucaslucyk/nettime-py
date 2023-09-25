386d6632087c417b6d6a85eefcdb7606d9b870d7
Lucas Lucyk <llucyk@grupospec.com>
2023-09-25 15:30:58 -0300

    field alias on base class

--- nettime_py/schemas/base.py
+++ nettime_py/schemas/base.py
@@ -1,15 +1,31 @@
-from typing import Optional, TypeVar
+from typing import Optional, Sequence, Tuple, TypeVar, Union
 from typing import Optional
 from pydantic import BaseModel, Field
 
 
 class Base(BaseModel):
-    id: Optional[int] = Field(default=None)
-    name: Optional[str] = Field(default=None)
+    id: Optional[int] = Field(default=None, alias="id")
+    name: Optional[str] = Field(default=None, alias="name")
 
     @property
     def _extra(self) -> set[str]:
         return set(self.__dict__) - set(self.__fields__)
+
+    @property
+    def alias_fields(self) -> Sequence[Union[str, None]]:
+        # for field in self.model_fields.values():
+        #     yield getattr(field, "alias", None)
+        return tuple(
+            getattr(field, "alias", None)
+            for field in self.model_fields.values()
+        )
+    
+    @classmethod
+    def get_alias_fields(cls) -> Sequence[Union[str, None]]:
+        return tuple(
+            getattr(field, "alias", None)
+            for field in cls.model_fields.values()
+        )
 
     class Config:
         extra = "allow"
